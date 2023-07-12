836c1e51a30e6dda019dd10c133dd21f0be57534
kolat94 <kolat.imap@gmail.com>
2023-07-12 12:26:20 +0200

    Version 1.0.2 initial

--- PlasmaDB/main.cpp
+++ PlasmaDB/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    a.setApplicationVersion("1.0.0");
+    a.setApplicationVersion("1.0.2");
 
     w.show();
     return a.exec();
