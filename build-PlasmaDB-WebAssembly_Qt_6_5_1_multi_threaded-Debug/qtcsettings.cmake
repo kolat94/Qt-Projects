6b48658dcd9233f1964ab397f4009889d0abcdb4
kolat <33928606+kolat94@users.noreply.github.com>
2023-07-13 20:55:58 +0200

    synchonisation maintenance

---
+++ build-PlasmaDB-WebAssembly_Qt_6_5_1_multi_threaded-Debug/qtcsettings.cmake
@@ -0,0 +1,11 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "E:/Github/Qt-Projects/build-PlasmaDB-WebAssembly_Qt_6_5_1_multi_threaded-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "E:/Qt/6.5.1/wasm_multithread/bin/qmake.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "E:/Qt/6.5.1/wasm_multithread" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "E:/emsdk/upstream/emscripten/emcc.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "E:/emsdk/upstream/emscripten/em++.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "E:/Qt/6.5.1/wasm_multithread/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
